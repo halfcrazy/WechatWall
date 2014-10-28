1d48bbe75f6c7196f4290fa818c01e466f0cc223
halfcrazy <hackzhuyan@gmail.com>
2014-10-28 23:48:51 +0800

    finish ApiCategoryHandler, ApiDetailHandler, ApiReceiveHandler. Have not been tested.

--- WechatWall/views/handlers.py
+++ WechatWall/views/handlers.py
@@ -6,7 +6,7 @@ import tornado.web
 import tornado.websocket
 from ..utils import object_to_json, generate_name
 from ..models import *
-from structure import InitData, CloseData
+from structure import InitData, CloseData, SyncData
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -28,7 +28,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     @staticmethod
     def broadcast(message):
         for client in WSHandler.clients:
-            client.write_message(message)
+            client.write_message(object_to_json(SyncData(message)))
 
     def get_compression_options(self):
         # Non-None enables compression with default options.
@@ -50,46 +50,117 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
 
 class ApiCategoryHandler(BaseHandler):
-    default_show_nums = 18
+    default_show_nums = 10
 
     def get(self,category_id):
-        category_ids = self.session.query(Category.id)
-        if category_id not in category_ids:
+        self.category_ids = self.session.query(Category.id).all()
+        if category_id not in self.category_ids:
             #主题分类不存在
+            print category_id, u"该主题id不存在"
             raise tornado.web.HTTPError(404)
 
         self.page = self.get_argument("p",1)
         if self.page < 1:
             #页数错误
+            print self.page, u"页数错误"
             raise tornado.web.HTTPError(404)
+
         self.page = self.page - 1
 
         if category_id == 0:
-            content_list = self.session.query(Content.id, Conetnt.category_id, Content.content, Content.created_at) \
-                               .offset(default_show_nums * self.page) \
-                               .limit(default_show_nums)
+            post_list = self.session.query(Post.id, Post.category_id, Post.content, Post.author, Post.created_at) \
+                                    .order_by(Post.id.desc()) \
+                                    .offset(default_show_nums * self.page) \
+                                    .limit(default_show_nums) \
+                                    .all()
         else:
-            content_list = self.session.query(Content.id, Conetnt.category_id, Content.content, Content.created_at) \
-                               .filter(Content.category_id == category_id) \
-                               .offset(default_show_nums * self.page) \
-                               .limit(default_show_nums)
+            post_list = self.session.query(Post.id, Post.category_id, Post.content, Post.created_at) \
+                                    .filter(Post.category_id == category_id) \
+                                    .order_by(Post.id.desc()) \
+                                    .offset(default_show_nums * self.page) \
+                                    .limit(default_show_nums) \
+                                    .all()
     
-        content_rs = [dict({'id': i[0], 'category_id': i[1], 'content': i[2].encode('utf-8'), 'created_at': i[3]}) for i in content_list]
+        post_rs = [dict({'id': i[0], 'category_id': i[1], 'content': i[2].encode('utf-8'), 'created_at': i[3]}) for i in post_list]
         
-        top_list = self.session.query(Top.id) \
-                               .filter(Top.category_id == category_id)
+        top_list = self.session.query(Top.id).filter(Top.category_id == category_id).all()
         
         top_rs = [dict({'id': i[0]}) for i in top_list]
 
         self.set_header("Content-Type","application/json")
-        self.write(json.dumps({'contents': content_rs, 'top': top_rs}, ensure_ascii=False))
+        self.finish(json.dumps({'posts': post_rs, 'top': top_rs}, ensure_ascii=False))
+        return
 
 
 class ApiDetailHandler(BaseHandler):
-    def get(self,contents_id):
-        self.render("index.html")
+    def get(self,content_id):
+        post = self.session.query(Content.id, Content.category_id, Content.content, Content.created_at) \
+                           .filter(Content.id == content_id) \
+                           .one()
+        comment_list = self.session.query(Comment.id, Comment.comment, Comment.author, Comment.reply_to, Comment.created_at) \
+                                   .filter(Comment.reply_to == content_id) \
+                                   .order_by(Comment.created_at) \
+                                   .all()
+        
+        post_rs = [dict({'id': i[0], 'category_id': i[1], 'content': i[2].encode('utf-8'), 'created_at': i[3]}) for i in post_list]
+        comment_rs = [dict({'id': i[0], 'comment': i[1].encode('utf-8'), 'author': i[2].encode('utf-8'), 'reply_to': i[3], 'created_at': i[4]}) for i in comment_list]
+        
+        self.set_header("Content-Type","application/json")
+        self.finish(json.dumps({'post': post_rs, 'comments': comment_rs}, ensure_ascii=False))
+        return
 
 
 class ApiReceiveHandler(BaseHandler):
     def post(self):
-        self.render("index.html")
+        #assert Content-Type to application/json
+        self.set_header("Content-Type","application/json")
+        
+        kind = self.get_argument("kind","")
+        if kind == "post":
+            try:
+                message = self.get_argument("message","")
+                if not message.strip():
+                    self.finish(json.dumps({'error': u'内容不能为空'}, ensure_ascii=False))
+                    return
+            
+                author = self.get_argument("author","")
+                if not author.strip():
+                    author = generate_name()
+            
+                category = self.get_argument("category","")
+                if not category.isdigit():
+                    self.finish(json.dumps({'error': u'分类不能为空'}, ensure_ascii=False))
+                    return
+                
+                self.post = Post(category_id=category, content=message, author=author)
+                self.session.add(self.post)
+                self.session.commit()
+
+                self.finish(json.dumps({'status': u'post创建成功'}, ensure_ascii=False))
+                return
+            
+            except Exception, e:
+                raise e
+        
+        elif kind == "comment":
+            try:
+                comment = self.get_argument("comment","")
+                if not comment.strip():
+                    self.finish(json.dumps({'error': u'内容不能为空'}, ensure_ascii=False))
+                    self.finish()
+                    return
+
+                author = self.get_argument("author","")
+                if not author.strip():
+                    author = generate_name()
+                reply_to = self.get_argument("reply_to","")
+            
+                self.comment = Comment(comment=comment, author=author, reply_to=reply_to)
+                self.session.add(self.comment)
+                self.session.commit()
+
+                self.finish(json.dumps({'status': u'comment创建成功'}, ensure_ascii=False))
+                return
+
+            except Exception, e:
+                raise e
