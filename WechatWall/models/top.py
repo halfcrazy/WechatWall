41ba67e1932376246847f416c491af52a949649d
halfcrazy <hackzhuyan@gmail.com>
2014-10-28 14:55:55 +0800

    add model top which is used to store those on top.

---
+++ WechatWall/models/top.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sqlalchemy import Column, Integer
+from ._base import Base
+
+class Top(Base):
+    __tablename__ = 'top'
+    id = Column(Integer, primary_key=True)
+    category_id = Column(Integer)
+
+    def __repr__(self):
+        return '<topid %r top_category %r>' % (self.id, self.category_id)
